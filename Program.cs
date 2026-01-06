c98acf71989bbd5c9c8234b28dd4c8ef01fcc7ff
Kenzo Friás <kenzofrias16@gmail.com>
2026-01-06 19:39:50 -0300

    final dos operadores condicionais

--- Program.cs
+++ Program.cs
@@ -3,26 +3,84 @@ using System.Runtime.Serialization;
 using aulas.classes.aula001;
 
 
-int quantidadeEmEstoque = 10;
-int quantidadeCompra = 0;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra && quantidadeCompra > 0;
+Console.WriteLine("Digite uma letra.");
+string letra = Console.ReadLine();
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade para compra: {quantidadeCompra}");
-Console.WriteLine($"\nÉ possível realizar a compra? {possivelVenda}");
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal!");
+        break;
+    default:
+        Console.WriteLine("Não é uma vogal!");
+        break;
+}
 
-if (quantidadeCompra == 0)
-{
-    Console.WriteLine("\nVenda inválida!");
-}
-else if (possivelVenda)
-{
-    Console.WriteLine("\nVenda realizada!");
-}
-else
-{
-    Console.WriteLine("\nDesculpe, não temos a quantidade desejada em estoque.");
-}
+
+// if(letra == "a" || 
+//     letra == "e" || 
+//     letra == "i" || 
+//     letra == "o" || 
+//     letra == "u")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal!");
+// }
+
+
+// if(letra == "a")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else if (letra == "e")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else if (letra == "i")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else if (letra == "o")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else if (letra == "u")
+// {
+//     Console.WriteLine("Vogal!");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal!");
+// }
+
+
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra && quantidadeCompra > 0;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade para compra: {quantidadeCompra}");
+// Console.WriteLine($"\nÉ possível realizar a compra? {possivelVenda}");
+
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("\nVenda inválida!");
+// }
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("\nVenda realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("\nDesculpe, não temos a quantidade desejada em estoque.");
+// }
 
 
 
